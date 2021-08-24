2cf63c85b80c5970d2c0d7128f0a68887764ebfb
franciscoruiz00 <franciscoruiz@hotmail.com>
2021-08-24 17:12:15 -0300

    commit inicial

---
+++ Persona.java
@@ -0,0 +1,16 @@
+
+public class Persona {
+	private String name;
+	
+	public Persona ( String nombre) {
+		nombre = name;
+	}
+
+	public String getName () {
+		return name;
+	}
+	 public String setName(String nombre) {
+		return nombre;
+		 
+	 }
+}
---
+++ Prueba.java
@@ -0,0 +1,8 @@
+
+public class Prueba {
+
+	public static void main ( String args[]) {
+		Persona p = new Persona ("Fran");
+		System.out.println("nombre :" +p.getName());
+	}
+}
